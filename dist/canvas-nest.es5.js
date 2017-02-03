535d28880e434b65351b392b7af1b82482b170df
F-loat <945852046@qq.com>
2017-02-03 20:28:19 +0800

    use es6 syntax

--- dist/canvas-nest.es5.js
+++ dist/canvas-nest.es5.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Copyright (c) 2016 hustcc
@@ -9,112 +9,175 @@
 
 !function () {
   // 封装方法，压缩之后减少文件大小
-  function get_attribute(node, attr, default_value) {
+  var get_attribute = function get_attribute(node, attr, default_value) {
     return node.getAttribute(attr) || default_value;
-  }
+  };
+
   // 封装方法，压缩之后减少文件大小
-  function get_by_tagname(name) {
+  var get_by_tagname = function get_by_tagname(name) {
     return document.getElementsByTagName(name);
-  }
+  };
+
   // 获取配置参数
   function get_config_option() {
-    var scripts = get_by_tagname("script"),
-        script_len = scripts.length,
-        script = scripts[script_len - 1]; // 当前加载的script
+    var scripts = get_by_tagname('script');
+    var script_len = scripts.length;
+    var script = scripts[script_len - 1]; //当前加载的script
     return {
-      l: script_len, // 长度，用于生成id用
-      z: get_attribute(script, "zIndex", -1), // z-index
-      o: get_attribute(script, "opacity", 0.5), // opacity
-      c: get_attribute(script, "color", "0,0,0"), // color
-      n: get_attribute(script, "count", 99) // count
+      l: script_len, //长度，用于生成id用
+      z: get_attribute(script, 'zIndex', -1), //z-index
+      o: get_attribute(script, 'opacity', 0.5), //opacity
+      c: get_attribute(script, 'color', '0,0,0'), //color
+      n: get_attribute(script, 'count', 99) //count
     };
   }
+
   // 设置canvas的高宽
   function set_canvas_size() {
-    canvas_width = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, canvas_height = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    canvas_width = the_canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    canvas_height = the_canvas.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   }
 
   // 绘制过程
   function draw_canvas() {
     context.clearRect(0, 0, canvas_width, canvas_height);
     // 随机的线条和当前位置联合数组
-    var e, i, d, x_dist, y_dist, dist; // 临时节点
+    var e = void 0,
+        i = void 0,
+        d = void 0,
+        x_dist = void 0,
+        y_dist = void 0,
+        dist = void 0; //临时节点
     // 遍历处理每一个点
-    random_points.forEach(function (r, idx) {
-      r.x += r.xa, r.y += r.ya, // 移动
-      r.xa *= r.x > canvas_width || r.x < 0 ? -1 : 1, r.ya *= r.y > canvas_height || r.y < 0 ? -1 : 1, // 碰到边界，反向反弹
-      context.fillRect(r.x - 0.5, r.y - 0.5, 1, 1); // 绘制一个宽高为1的点
-      // 从下一个点开始
-      for (i = idx + 1; i < all_array.length; i++) {
-        e = all_array[i];
-        // 当前点存在
-        if (null !== e.x && null !== e.y) {
-          x_dist = r.x - e.x; // x轴距离 l
-          y_dist = r.y - e.y; // y轴距离 n
-          dist = x_dist * x_dist + y_dist * y_dist; // 总距离, m
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
 
-          dist < e.max && (e === current_point && dist >= e.max / 2 && (r.x -= 0.03 * x_dist, r.y -= 0.03 * y_dist), // 靠近的时候加速
-          d = (e.max - dist) / e.max, context.beginPath(), context.lineWidth = d / 2, context.strokeStyle = "rgba(" + config.c + "," + (d + 0.2) + ")", context.moveTo(r.x, r.y), context.lineTo(e.x, e.y), context.stroke());
+    try {
+      for (var _iterator = random_points[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var r = _step.value;
+
+        r.x += r.xa;
+        r.y += r.ya; //移动
+        r.xa *= r.x > canvas_width || r.x < 0 ? -1 : 1;
+        r.ya *= r.y > canvas_height || r.y < 0 ? -1 : 1; // 碰到边界，反向反弹
+        context.fillRect(r.x - 0.5, r.y - 0.5, 1, 1); // 绘制一个宽高为1的点
+        // 遍历处理沾附关系
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
+
+        try {
+          for (var _iterator2 = all_array[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var _e = _step2.value;
+
+            x_dist = r.x - _e.x; // x轴距离 l
+            y_dist = r.y - _e.y; // y轴距离 n
+            dist = x_dist * x_dist + y_dist * y_dist; // 总距离, m
+            // 当前点在沾附范围内
+            if (null !== _e.x && null !== _e.y && dist < _e.max) {
+              _e === current_point && dist >= _e.max / 2 && (r.x -= 0.03 * x_dist, r.y -= 0.03 * y_dist); // 靠近的时候加速
+              d = (_e.max - dist) / _e.max;
+              context.beginPath();
+              context.lineWidth = d / 2;
+              context.strokeStyle = 'rgba(' + config.c + ', ' + (d + 0.2) + ')';
+              context.moveTo(r.x, r.y);
+              context.lineTo(_e.x, _e.y);
+              context.stroke();
+            }
+          }
+        } catch (err) {
+          _didIteratorError2 = true;
+          _iteratorError2 = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+              _iterator2.return();
+            }
+          } finally {
+            if (_didIteratorError2) {
+              throw _iteratorError2;
+            }
+          }
         }
       }
-    }), frame_func(draw_canvas);
+    } catch (err) {
+      _didIteratorError = true;
+      _iteratorError = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+          _iterator.return();
+        }
+      } finally {
+        if (_didIteratorError) {
+          throw _iteratorError;
+        }
+      }
+    }
+
+    frame_func(draw_canvas);
   }
+
   // 创建画布，并添加到body中
-  var the_canvas = document.createElement("canvas"),
-      // 画布
-  config = get_config_option(),
-      // 配置
-  canvas_id = "c_n" + config.l,
-      // canvas id
-  context = the_canvas.getContext("2d"),
-      canvas_width,
-      canvas_height,
-      frame_func = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (func) {
-    window.setTimeout(func, 1000 / 45);
-  },
-      random = Math.random,
-      current_point = {
+  var the_canvas = document.createElement('canvas'); // 画布
+  var config = get_config_option(); // 配置
+  var canvas_id = 'c_n' + config.l; // canvas id
+  var context = the_canvas.getContext('2d');
+  var frame_func = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (func) {
+    return window.setTimeout(func, 1000 / 45);
+  };
+  var random = Math.random;
+  var current_point = {
     x: null, // 当前鼠标x
     y: null, // 当前鼠标y
-    max: 20000 // 圈半径的平方
-  },
-      all_array;
+    max: 20000 };
+  var canvas_width = void 0,
+      canvas_height = void 0,
+      all_array = void 0;
+  var random_points = [];
+
   the_canvas.id = canvas_id;
-  the_canvas.style.cssText = "position:fixed;top:0;left:0;z-index:" + config.z + ";opacity:" + config.o;
-  get_by_tagname("body")[0].appendChild(the_canvas);
+  the_canvas.style.cssText = 'position:fixed;top:0;left:0;z-index:' + config.z + ';opacity:' + config.o;
+  get_by_tagname('body')[0].appendChild(the_canvas);
 
   // 初始化画布大小
   set_canvas_size();
-  window.onresize = set_canvas_size;
-  // 当时鼠标位置存储，离开的时候，释放当前位置信息
-  window.onmousemove = function (e) {
-    e = e || window.event;
+
+  // 监听窗口尺寸变化
+  window.addEventListener('resize', set_canvas_size);
+
+  // 鼠标移动时位置存储，离开时释放位置信息
+  window.addEventListener('mousemove', function () {
+    var e = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : window.event;
+
     current_point.x = e.clientX;
     current_point.y = e.clientY;
-  }, window.onmouseout = function () {
+  });
+  window.addEventListener('mouseout', function () {
     current_point.x = null;
     current_point.y = null;
-  };
+  });
+
   // 随机生成config.n条线位置信息
-  for (var random_points = [], i = 0; config.n > i; i++) {
-    var x = random() * canvas_width,
-        // 随机位置
-    y = random() * canvas_height,
-        xa = 2 * random() - 1,
-        // 随机运动方向
-    ya = 2 * random() - 1;
+  for (var i = 0; config.n > i; i++) {
+    var x = random() * canvas_width; // 随机位置
+    var y = random() * canvas_height;
+    var xa = 2 * random() - 1; // 随机运动方向
+    var ya = 2 * random() - 1;
     // 随机点
     random_points.push({
       x: x,
       y: y,
       xa: xa,
       ya: ya,
-      max: 6000 // 沾附距离
-    });
+      max: 6000 });
   }
+
   all_array = random_points.concat([current_point]);
+
   // 0.1秒后绘制
   setTimeout(function () {
-    draw_canvas();
+    return draw_canvas();
   }, 100);
 }();
