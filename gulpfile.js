6643e156b5542cbff8093c367f2f0f7d8c86cbad
F-loat <945852046@qq.com>
2017-02-03 18:17:21 +0800

    add gulp-babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,20 @@
-const fs = require('fs');
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
 const injectVersion = require('gulp-inject-version');
 
-gulp.task('mini', () => (
-  gulp.src('src/canvas-nest.js')
-  .pipe(injectVersion())
-  .pipe(gulp.dest('dist/'))
-  .pipe(uglify({
-  	preserveComments: 'license'
-  }))    //uglify
-  .pipe(rename("canvas-nest.min.js"))
-  .pipe(gulp.dest('dist/'))
+gulp.task('default', () => (
+  gulp.src('src/index.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(injectVersion())
+    .pipe(rename("canvas-nest.es5.js"))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify({
+    	preserveComments: 'license'
+    }))    //uglify
+    .pipe(rename("canvas-nest.min.js"))
+    .pipe(gulp.dest('dist'))
 ));
